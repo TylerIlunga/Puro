03a0cf51b2aa13c42587fd29d172ca03c26f43f9
Tyler Ilunga <tylerilunga1@gmail.com>
2020-05-01 18:07:32 -0700

    refactoring/cleaning(Google/OAUTH)

--- api/src/controllers/OAuth/Google/index.js
+++ api/src/controllers/OAuth/Google/index.js
@@ -46,7 +46,7 @@ const linkAccount = async (userId, token, res) => {
     },
   });
   if (linkedAccount) {
-    console.log('Linked Account already on file for (Google)');
+    console.log('Linked Account already on file: (Google)');
     return res.json({
       success: false,
       error: 'Your Google Account has been linked.',
@@ -61,15 +61,18 @@ const linkAccount = async (userId, token, res) => {
   })
     .then((result) => {
       console.log('linkedAccount.create result', result);
+
       account = null;
       delete account;
-      let msg = { success: true, error: false };
-      return res.send(config.broadcastChannel(msg));
+
+      return res.send(config.broadcastChannel({ success: true, error: false }));
     })
     .catch((error) => {
       console.log('linkedAccount.create error', error);
+
       account = null;
       delete account;
+
       return res.json({
         success: false,
         error: 'Error linking account. Contact Support.',
@@ -80,7 +83,8 @@ const linkAccount = async (userId, token, res) => {
 module.exports = {
   /**
    * oauth[GET]
-   * Redirects current user to Google's authentication view.
+   * @param {Object} req
+   * @param {Object} res
    */
   oauth(req, res) {
     if (req.query && req.query.connect && req.query.uid) {
@@ -111,14 +115,14 @@ module.exports = {
   },
   /**
    * callback[GET]
-   * Gathers current user's data allowed by Google, organizes that data,
-   * and stores it for future analysis.
+   * @param {Object} req
+   * @param {Object} res
    */
   async callback(req, res) {
-    console.log('req.query', req.query);
     if (!(account && req.query.code && req.query)) {
       return res.json({ error: 'OAuth required!', success: false });
     }
+
     const oauth2Data = await oauth2Client.getToken(req.query.code);
     if (!oauth2Data.tokens) {
       return res.json({
@@ -126,19 +130,20 @@ module.exports = {
         success: false,
       });
     }
-    console.log('tokens', oauth2Data.tokens);
+
+    console.log('callback() Tokens:', oauth2Data.tokens);
+
     //** Linking (NOT CAMPAIGN RELATED) */
     if (account.linking && account.user_id) {
       return linkAccount(account.user_id, oauth2Data.tokens, res);
     }
 
     oauth2Client.setCredentials(oauth2Data.tokens);
+
     const oauth2 = google.oauth2({
       auth: oauth2Client,
       version: 'v2',
     });
-    console.log('oauth2', oauth2);
-
     oauth2.userinfo.v2.me.get(async (err, profile) => {
       if (err) {
         return res.json({
@@ -147,21 +152,22 @@ module.exports = {
         });
       }
       const googleData = profile.data;
-      console.log('googleData', googleData);
+      console.log('googleData:', googleData);
+
       const userEntry = await Entry.findOne({
         where: { company_id: googleData.id },
       });
       if (userEntry) {
-        console.log('entry exists:::::', userEntry);
+        console.log('Entry exists (Update)');
         return userEntry
           .update({ clicks: userEntry.dataValues.clicks + 1 })
-          .then((result) => res.redirect(account.redirect_uri))
+          .then((_) => res.redirect(account.redirect_uri))
           .catch((error) => {
             console.log('userEntry.update() error', error);
-            // return res.json({ error: "Error updating clicks count!", success: false });
-            return res.redirect(account.redirect_uri);
+            res.redirect(account.redirect_uri);
           });
       }
+
       Entry.create({
         company_id: googleData.id,
         email: googleData.email,
@@ -170,24 +176,22 @@ module.exports = {
       })
         .then(async (entry) => {
           console.log('success saving entry', entry);
+
           res.redirect(account.redirect_uri);
-          const user = await User.findOne({ where: { id: 1 } });
-          // if (!user || user.subscription === "free") {
-          //   return console.log('No user or subscription === free for account: ', account.user_id);
-          // }
-          const analyticalLog = {
+
+          account['entry_id'] = entry.dataValues.id;
+
+          gatherAnalytics(req, account, 'google', {
             ...googleData,
             access_token: oauth2Data.tokens.access_token,
             refresh_token: oauth2Data.tokens.refresh_token,
             token_expiry_date: oauth2Data.tokens.expiry_date,
             ip: req.ip,
             link: account.redirect_uri,
-          };
-          console.log('analyticalLog', analyticalLog);
-          account['entry_id'] = entry.dataValues.id;
-          gatherAnalytics(req, account, 'google', analyticalLog);
+          });
         })
         .catch((error) => {
+          console.log('Error creating/saving new entry:', error);
           return res.json({ error: 'Error saving entry!', success: false });
         });
     });
